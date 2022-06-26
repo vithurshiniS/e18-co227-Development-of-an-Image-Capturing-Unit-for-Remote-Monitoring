676c10839bda1b123668aa2f2c3f76f58e8d43cd
vilakshan2996 <vilakshan2996@gmail.com>
2022-06-26 09:05:57 +0530

    Update Flutter MQTT Client

--- fluttermqtt/lib/main.dart
+++ fluttermqtt/lib/main.dart
@@ -1,115 +1,268 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:typed_data';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttermqtt/secrets.dart';
+import 'package:image/image.dart' as img;
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:ndialog/ndialog.dart';
+
+
+void main(){
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'MQTT ESP32CAM VIEWER',
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: MQTTClient(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MQTTClient extends StatefulWidget {
+  const MQTTClient({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MQTTClientState createState() => _MQTTClientState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MQTTClientState extends State<MQTTClient> {
+  String statusText = "Status Text";
+  bool isConnected = false;
+  TextEditingController idTextController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final MqttServerClient client =
+      MqttServerClient(BROKERURL, 'flutterapp');
+
+  @override
+  void dispose() {
+    idTextController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var width = MediaQuery.of(context).size.width;
+    final bool hasShortWidth = width < 600;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          children: [header(), body(hasShortWidth), footer()],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget header() {
+    return Expanded(
+      child: Container(
+        child: Center(
+          child: Text(
+            'ESP32CAM Viewer\n- AWS IoT -',
+            style: TextStyle(
+                fontSize: 28, color: Colors.white, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      flex: 3,
+    );
+  }
+
+  Widget body(bool hasShortWidth) {
+    return Expanded(
+      child: Container(
+        child: hasShortWidth
+            ? Column(
+                children: [bodyMenu(), Expanded(child: bodySteam())],
+              )
+            : Row(
+                children: [
+                  Expanded(
+                    child: bodyMenu(),
+                    flex: 2,
+                  ),
+                  Expanded(
+                    child: bodySteam(),
+                    flex: 8,
+                  )
+                ],
+              ),
+      ),
+      flex: 20,
+    );
+  }
+
+  Widget bodyMenu() {
+    return Container(
+      color: Colors.black26,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: TextFormField(
+                enabled: !isConnected,
+                controller: idTextController,
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'MQTT Client Id',
+                    labelStyle: TextStyle(fontSize: 10),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.subdirectory_arrow_left),
+                      onPressed: _connect,
+                    ))),
+          ),
+          isConnected
+              ? TextButton(onPressed: _disconnect, child: Text('Disconnect'))
+              : Container()
+        ],
+      ),
+    );
+  }
+
+  Widget bodySteam() {
+    return Container(
+      color: Colors.black,
+      child: StreamBuilder(
+        stream: client.updates,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            );
+          else {
+            final mqttReceivedMessages =
+                snapshot.data as List<MqttReceivedMessage<MqttMessage?>>?;
+            print("mqtt recived messages ${mqttReceivedMessages![0]}");
+
+            final recMess =
+                mqttReceivedMessages[0].payload as MqttPublishMessage;
+            print(recMess.payload.message);
+
+            img.Image jpegImage = img.decodeJpg(recMess.payload.message) as img.Image;
+
+            print(
+                'img width = ${jpegImage.width}, height = ${jpegImage.height}');
+            return Image.memory(
+              img.encodeJpg(jpegImage) as Uint8List,
+              gaplessPlayback: true,
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget footer() {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 16),
+        child: Text(
+          statusText,
+          style: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.amberAccent),
+        ),
+      ),
+      flex: 1,
+    );
+  }
+
+  _connect() async {
+    if (idTextController.text.trim().isNotEmpty) {
+      ProgressDialog progressDialog = ProgressDialog(context,
+          blur: 0,
+          dialogTransitionType: DialogTransitionType.Shrink,
+          dismissable: false);
+      progressDialog.setLoadingWidget(CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.red),
+      ));
+      progressDialog
+          .setMessage(Text("Please Wait, Connecting to AWS IoT MQTT Broker"));
+      progressDialog.setTitle(Text("Connecting"));
+      progressDialog.show();
+
+      isConnected = await mqttConnect(idTextController.text.trim());
+      progressDialog.dismiss();
+    }
+  }
+
+  _disconnect() {
+    client.disconnect();
+  }
+
+  Future<bool> mqttConnect(String uniqueId) async {
+    setStatus("Connecting MQTT Broker");
+
+    // After adding your certificates to the pubspec.yaml, you can use Security Context.
+    //
+    ByteData rootCA = await rootBundle.load('assets/certs/RootCA.pem');
+    // ByteData deviceCert =
+    //     await rootBundle.load('assets/certs/DeviceCertificate.crt');
+    // ByteData privateKey = await rootBundle.load('assets/certs/Private.key');
+    
+    SecurityContext context = SecurityContext.defaultContext;
+    context.setClientAuthoritiesBytes(rootCA.buffer.asUint8List());
+    // context.useCertificateChainBytes(deviceCert.buffer.asUint8List());
+    // context.usePrivateKeyBytes(privateKey.buffer.asUint8List());
+    
+    client.securityContext = context;
+
+
+    client.logging(on: true);
+    client.keepAlivePeriod = 20;
+    client.port = 8883;
+    client.secure = true;
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    client.pongCallback = pong;
+
+    final MqttConnectMessage connMess =
+        MqttConnectMessage().withClientIdentifier(uniqueId).authenticateAs(USERNAME,PWD).startClean();
+    client.connectionMessage = connMess;
+    print("Conn message is $connMess");
+
+    await client.connect();
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print("Connected to AWS Successfully!");
+    } else {
+
+      return false;
+    }
+
+    const topic = 'esp32/cam_0';
+    client.subscribe(topic, MqttQos.atMostOnce);
+
+    return true;
+  }
+
+  void setStatus(String content) {
+    setState(() {
+      statusText = content;
+    });
+  }
+
+  void onConnected() {
+    setStatus("Client connection was successful");
+  }
+
+  void onDisconnected() {
+    setStatus("Client Disconnected");
+    isConnected = false;
+  }
+
+  void pong() {
+    print('Ping response client callback invoked');
   }
 }
